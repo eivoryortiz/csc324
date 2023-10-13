be6c628969fa5feed579714941844b44902b393a
eivoryortiz <142541083+eivoryortiz@users.noreply.github.com>
2023-10-12 23:06:16 -0400

    "table with small code"

---
+++ custom.js
@@ -0,0 +1,37 @@
+/********************************************************************
+ * 
+ * Your introductory remarks go here.
+ * 
+ *******************************************************************/
+
+ // The array of objects, one object for each artist.
+ 
+ const artists = [
+   {
+     name: "Ms Scandalous",
+     birthYear: 1985,
+     link: "https://www.youtube.com/watch?v=2FPivlfvxu0"
+   },
+   {
+    name: "Juggy D",
+    birthYear: 1981,
+    link: "https://www.youtube.com/watch?v=1jAc_-FVjdI"
+  },
+  {
+    name: "Sukhbir Singh",
+    birthYear: 1969,
+    link: "https://www.youtube.com/watch?v=HiprNF9Jad0"
+  },
+  {
+    name: "Abrar-ul-Haq",
+    birthYear: 1989,
+    link: "https://www.youtube.com/watch?v=-lnnVIP7FEc"
+  },
+  {
+    name: "Rishi Rich",
+    birthYear: 1970,
+    link: "https://www.youtube.com/watch?v=O95-w2gACuA"
+  }
+ ]
+
+ // complete with code to select and populate the table
